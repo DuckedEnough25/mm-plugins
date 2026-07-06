2e4bbe27c78a1a9e8db30482571df4caaafc4647
sebkuip <kuipers.sebastian@ziggo.nl>
2026-07-06 21:08:33 +0200

    Fix message forwarding/editing exploits

--- MNFH-dm-limiter/MNFH-dm-limiter.py
+++ MNFH-dm-limiter/MNFH-dm-limiter.py
@@ -1,20 +1,30 @@
 from discord.ext import commands
 
+
+def message_check(message):
+    if message.author.bot:
+        return False
+    elif message.channel.id not in (721750845749723236, 1065831118970040340):
+        return False
+    elif message.author.guild_permissions.manage_messages:
+        return False
+    return len(message.content + message.message_snapshots[0].content) > 100
+
 class MNFHDMLimiter(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.author.bot:
-            return
-        elif message.channel.id not in (721750845749723236, 1065831118970040340):
-            return
-        elif message.author.guild_permissions.manage_messages:
-            return
-        elif len(message.content) > 100:
+        if message_check(message):
             await message.reply("This channel is only for asking people to DM with you. Intro's or other long messages are not allowed.", delete_after=10)
             await message.delete()
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, before, after):
+        if message_check(after):
+            await after.reply("This channel is only for asking people to DM with you. Intro's or other long messages are not allowed.", delete_after=10)
+            await after.delete()
 
 async def setup(bot):
     await bot.add_cog(MNFHDMLimiter(bot))
